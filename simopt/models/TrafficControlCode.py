c43335bacce7164da1034ab0b83538c3c33f170d
Wes <wchankin@ncsu.edu>
2022-05-05 09:13:13 -0400

    Update TrafficControlCode.py

--- simopt/models/TrafficControlCode.py
+++ simopt/models/TrafficControlCode.py
@@ -947,7 +947,9 @@ class MinWaitingTime(Problem):
         satisfies : bool
             indicates if solution `x` satisfies the deterministic constraints.
         """
-        return True         #will update
+        box_feasible = super().check_deterministic_constraints(x)
+
+
 
     def get_random_solution(self, rand_sol_rng):
         """
